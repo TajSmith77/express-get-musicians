739244dba63ce19b3bef719da4daa24ca73b9e36
Taj Smith <TajSmith@Deloitte.com>
2024-03-25 12:26:54 -0400

    created get for musicians and tests

--- index.test.js
+++ index.test.js
@@ -3,20 +3,20 @@ const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
 
-const request = require("supertest")
+const request = require("supertest");
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician } = require('./models/index');
 const app = require('./src/app');
 const seedMusician = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
-
-
-
-
-    
+    test("Testing musicians endpoint", async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData[0].name).toBe('Mick Jagger');
+        expect(responseData[1].instrument).toBe('Voice');
+    })  
 })
--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
-
+app.get("/musicians", async (request, response) => {
+    // WRITE YOUR CODE HERE
+    
+    let allMusicians = await Musician.findAll();    
+    response.json(allMusicians);
+    })
 
 
 

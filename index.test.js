d8ab2ab5e6c7d8f235d4455b3237a3c34041a48e
Taj Smith <TajSmith@Deloitte.com>
2024-03-25 20:22:23 -0400

    Created musicians and bands enpoints with tests

--- index.test.js
+++ index.test.js
@@ -18,5 +18,54 @@ describe('./musicians endpoint', () => {
         expect(response.statusCode).toBe(200);
         expect(responseData[0].name).toBe('Mick Jagger');
         expect(responseData[1].instrument).toBe('Voice');
+    }) 
+    test("Testing musicians 1 endpoint", async () => {
+        const response = await request(app).get("/musicians/1");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData.name).toBe('Mick Jagger');
+        expect(responseData.instrument).toBe('Voice');
+    }) 
+    test("Testing musicians 2 endpoint", async () => {
+        const response = await request(app).get("/musicians/2");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData.name).toBe('Drake');
+        expect(responseData.instrument).toBe('Voice');
+    }) 
+    test("Testing musicians 3 endpoint", async () => {
+        const response = await request(app).get("/musicians/3");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData.name).toBe('Jimi Hendrix');
+        expect(responseData.instrument).toBe('Guitar');
+    }) 
+    test("Testing bands endpoint", async () => {
+        const response = await request(app).get("/bands");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData[0].name).toBe('The Beatles');
+        expect(responseData[1].genre).toBe('Pop');
+    })  
+    test("Testing bands 1 endpoint", async () => {
+        const response = await request(app).get("/bands/1");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData.name).toBe('The Beatles');
+        expect(responseData.genre).toBe('Rock');
+    })  
+    test("Testing bands 2 endpoint", async () => {
+        const response = await request(app).get("/bands/2");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData.name).toBe('Black Pink');
+        expect(responseData.genre).toBe('Pop');
+    })  
+    test("Testing bands 3 endpoint", async () => {
+        const response = await request(app).get("/bands/3");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData.name).toBe('Coldplay');
+        expect(responseData.genre).toBe('Rock');
     })  
 })
--- src/app.js
+++ src/app.js
@@ -1,21 +1,47 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
-app.get("/musicians", async (request, response) => {
-    // WRITE YOUR CODE HERE
-    
+app.get("/musicians", async (request, response) => {    
     let allMusicians = await Musician.findAll();    
     response.json(allMusicians);
     })
+app.get("/musicians/1", async (request, response) => {        
+    let allMusicians = await Musician.findAll();
+    let thisMusician = allMusicians[0];    
+    response.json(thisMusician);
+    })
+app.get("/musicians/2", async (request, response) => {            
+    let allMusicians = await Musician.findAll();
+    let thisMusician = allMusicians[1];    
+    response.json(thisMusician);
+    })
+app.get("/musicians/3", async (request, response) => {                
+    let allMusicians = await Musician.findAll();
+    let thisMusician = allMusicians[2];    
+    response.json(thisMusician);
+    })
 
-
-
-
-
-
+app.get("/bands", async (request, response) => {        
+    let allBands = await Band.findAll();    
+    response.json(allBands);
+    })
+app.get("/bands/1", async (request, response) => {        
+    let allBands = await Band.findAll();   
+    let thisBand = allBands[0]; 
+    response.json(thisBand);
+    })
+app.get("/bands/2", async (request, response) => {        
+    let allBands = await Band.findAll();   
+    let thisBand = allBands[1]; 
+    response.json(thisBand);
+    })
+app.get("/bands/3", async (request, response) => {        
+    let allBands = await Band.findAll();   
+    let thisBand = allBands[2]; 
+    response.json(thisBand);
+    })
 module.exports = app;
